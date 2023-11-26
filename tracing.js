040e5835cbda3fcd989346c36553c4935ff43e5e
Harrison-Salzverg <73858241+Harrison-Salzverg@users.noreply.github.com>
2023-11-26 10:28:25 -0500

    Updated tracing.js to use Jaeger

--- tracing.js
+++ tracing.js
@@ -1,6 +1,6 @@
 const { Resource } = require("@opentelemetry/resources");
 const { SemanticResourceAttributes } = require("@opentelemetry/semantic-conventions");
-const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
+const { JaegerExporter } = require("@opentelemetry/exporter-jaeger");
 const { SimpleSpanProcessor } = require("@opentelemetry/sdk-trace-base");
 const { NodeTracerProvider } = require("@opentelemetry/sdk-trace-node");
 const { trace } = require("@opentelemetry/api");
@@ -11,13 +11,15 @@ const { HttpInstrumentation } = require("@opentelemetry/instrumentation-http");
 const { registerInstrumentations } = require("@opentelemetry/instrumentation");
 //Exporter
 module.exports = (serviceName) => {
-   const exporter = new ConsoleSpanExporter();
+    const jaegerExporter = new JaegerExporter({
+        serviceName: serviceName,
+    });
    const provider = new NodeTracerProvider({
        resource: new Resource({
            [SemanticResourceAttributes.SERVICE_NAME]: serviceName,
        }),
    });
-   provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
+   provider.addSpanProcessor(new SimpleSpanProcessor(jaegerExporter));
    provider.register();
    registerInstrumentations({
        instrumentations: [
